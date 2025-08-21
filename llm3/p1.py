02642ca64c04b8f4456f26945759baef064496ea
songhee121 <hehe9331@gmail.com>
2025-08-21 15:02:24 +0900

    250821

--- llm3/p1.py
+++ llm3/p1.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from myllm.MYLLM import geminiTxt
+
+from MYLLM import geminiTxt
 
 #Side Bar
 st.sidebar.markdown("Clicked Page 1")
@@ -10,8 +11,16 @@ text=st.text_area(label="질문입력: ",
                   placeholder="질문을 입력 하세요.")
 if st.button("SEND"): #버튼 클릭시
     if text: #텍스트 존재시
-        st.info(text)
-        result=geminiTxt(text)
+        # Progress Bar Start -----------------------------------------
+        progress_text = "Operation in progress. Please wait."
+        my_bar = st.progress(0, text=progress_text)
+        for percent_complete in range(100):
+            time.sleep(0.08)
+            my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(1)
+        # Progress Bar End -----------------------------------------
+
+        result=geminiTxt(text)  #result에 답변 저장
         st.info(result)
     else:
         st.info("질문을 입력 하세요.")
--- myllm/MYLLM.py
+++
@@ -1,6 +0,0 @@
-from myllm.MyAPI import geminiModel
-
-def geminiTxt(txt):
-    model = geminiModel()
-    response = model.generate_content(txt)
-    return(response.text)
