08a5f1e2a417662943ccd6916bbfbf0a11b98606
songhee121 <hehe9331@gmail.com>
2025-08-21 17:41:57 +0900

    250821

--- llm3/p4.py
+++ llm3/p4.py
@@ -8,7 +8,7 @@ from MYLLM import geminiTxt, geminiModel
 st.sidebar.markdown("Clicked Page 4")
 
 #Page
-st.title("Page 4: translate")
+st.title("Page 4: translator")
 
 text = st.text_area(label=" 질문입력: ", placeholder="질문을 입력하세요.")
 model=geminiModel()
