6a6120b8190b18b3e945e46a77b940ce648f9fb3
songhee121 <hehe9331@gmail.com>
2025-08-22 13:05:06 +0900

    250822

--- llm3/p6.py
+++ llm3/p6.py
@@ -13,11 +13,29 @@ if menu=="IMAGE":
     file=st.file_uploader("이미지 선택", type=["jpg", "png", "jpeg"])
     if file:
         save_uploadedfile("img", file, st)
+        st.download_button(
+            label="파일 다운로드",
+            data=file,
+            file_name=file.name,
+            mime="image/jpg"
+        )
 elif menu=="PDF":
     file=st.file_uploader("PDF 선택", type=["pdf"])
     if file:
         save_uploadedfile("pdf", file, st)
+        st.download_button(
+            label="파일 다운로드",
+            data=file,
+            file_name=file.name,
+            mime="application/pdf"
+        )
 elif menu=="CSV":
     file=st.file_uploader("CSV 선택", type=["CSV"])
     if file:
         save_uploadedfile("csv", file, st)
+        st.download_button(
+            label="파일 다운로드",
+            data=file,
+            file_name=file.name,
+            mime="text/text"
+        )
